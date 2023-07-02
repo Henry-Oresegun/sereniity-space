618980d8ba858d303507d21a48e266164de0fb1a
Henry-Oresegun45 <87371608+Henry-Oresegun@users.noreply.github.com>
2023-07-02 15:16:23 +0100

    Add files via upload

---
+++ images.js
@@ -0,0 +1,33 @@
+import bg from '../assets/bg.png';
+import fbathroom from '../assets/fbathroom.png'
+import bath1 from '../assets/bath1.png';
+import bath4 from '../assets/bath4.png';
+import garden from '../assets/garden.png';
+import logo from '../assets/logo.png';
+import airbnb from '../assets/airbnb.png';
+import masterbedroom from '../assets/masterbedroom.png';
+import ensuite from '../assets/ensuite.png'
+import doublebedroom from '../assets/doublebedroom.png'
+import largesinglebedroom from '../assets/largesinglebedroom.png'
+import livingroom from '../assets/livingroom.png'
+import hallway from '../assets/hallway.png'
+import kitchen from '../assets/kitchen.png';
+
+const images = {
+    bg,
+    fbathroom,
+    bath1,
+    bath4,
+    garden,
+    masterbedroom,
+    ensuite,
+    doublebedroom,
+    largesinglebedroom,
+    livingroom,
+    hallway,
+    kitchen,
+    logo,
+    airbnb
+  };
+
+export default images

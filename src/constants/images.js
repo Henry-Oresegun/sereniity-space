b7af363b2c9924b8746d7047718acb202e328ddb
Henry-Oresegun45 <87371608+Henry-Oresegun@users.noreply.github.com>
2023-07-02 15:21:52 +0100

    Add files via upload

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,32 +1,30 @@
 import bg from '../assets/bg.png';
+import fbathroom from '../assets/fbathroom.png'
 import bath1 from '../assets/bath1.png';
-import bath2 from '../assets/bath2.png';
-import bath3 from '../assets/bath3.png';
 import bath4 from '../assets/bath4.png';
-import bath5 from '../assets/bath5.png';
-import bath6 from '../assets/bath6.png';
-import bath7 from '../assets/bath7.png';
-import bath8 from '../assets/bath8.png';
 import garden from '../assets/garden.png';
 import logo from '../assets/logo.png';
 import airbnb from '../assets/airbnb.png';
 import masterbedroom from '../assets/masterbedroom.png';
+import ensuite from '../assets/ensuite.png'
 import doublebedroom from '../assets/doublebedroom.png'
+import largesinglebedroom from '../assets/largesinglebedroom.png'
+import livingroom from '../assets/livingroom.png'
+import hallway from '../assets/hallway.png'
 import kitchen from '../assets/kitchen.png';
 
 const images = {
     bg,
+    fbathroom,
     bath1,
-    bath2,
-    bath3,
     bath4,
-    bath5,
-    bath6,
-    bath7,
-    bath8,
     garden,
     masterbedroom,
+    ensuite,
     doublebedroom,
+    largesinglebedroom,
+    livingroom,
+    hallway,
     kitchen,
     logo,
     airbnb

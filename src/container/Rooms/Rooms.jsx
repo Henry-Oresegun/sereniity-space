9dc5ea070356372a817aced250fb69ea8ba7d287
Henry-Oresegun45 <87371608+Henry-Oresegun@users.noreply.github.com>
2023-07-13 20:28:19 +0100

    Add files via upload

--- src/container/Rooms/Rooms.jsx
+++ src/container/Rooms/Rooms.jsx
@@ -6,54 +6,112 @@ const Rooms = () => (
   
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app__wrapper_info' style={{ marginTop: '-30px' }}>
-  <h1 className='app__rooms-h1'>Living Room</h1>
-  <p className='p__opensans' style={{ margin: '1rem 0' }}>
-    - A vast living/family room with a large window that allows ample natural light to fill the space.<br />
-    - This room offers plenty of room for relaxation, entertainment, and quality time with loved ones.<br />
-    - The large window provides scenic views and enhances the overall ambiance of the room.<br />
-    - The spacious dining room is conveniently located and easily accessible from the large kitchen.<br />
-    - It offers a comfortable and inviting space for family meals, gatherings, and entertaining guests.<br />
-    - This combination creates a cohesive and functional area for dining and culinary activities.
-  </p>
-  <h1 className='app__rooms-h1' style={{margin: '2rem 0'}}>Kitchen </h1>
-    <p className='p__opensans' style={{margin: '-2rem 0'}}>
-  - The kitchen units are designed to be spacious, providing ample storage and workspace.<br />
-  - Top quality, built with attention to detail and using high-quality materials.<br />
-  - Incorporated with the latest technology, such as advanced appliances and smart features.<br />
-  - Custom lighting fixtures are installed, offering both ambient and task lighting to enhance its functionality.<br />
-</p>
-<h1 className='app__rooms-h1'style={{margin: '4rem 0'}}>Master Bedroom</h1>
-     <p className='p__opensans' style={{margin: '-4rem 0'}}>
-  - The master bedroom is designed to be a haven of peace and tranquility.<br />
-  - It features clean lines and a fresh white finish, creating a sense of serenity and calmness.<br />
-  - Large windows allow natural light to fill the room, creating a bright and airy ambiance during the day.<br />
-  - Spacious and provides maximum room for relaxation and restful sleep.<br />
-  - It is equipped with en-suite amenities, offering convenient and private bathroom facilities.<br />
-  - The en-suite amenities include features such as a luxurious bathtub, a modern shower, and a stylish vanity area.<br/>
-</p>
-<h1 className='app__rooms-h1' style={{margin: '5rem 0'}}>Double Bedroom</h1>
-     <p className='p__opensans' style={{margin: '-5rem 0'}}>
-  - The double bedroom is designed with a modern and stylish aesthetic, featuring contemporary furniture and décor.<br />
-  - The room design incorporates sound-absorbing elements, reducing noise and promoting a peaceful environment..<br />
-  - Includes adjustable lighting options, allowing for creating different moods and ambiance.<br />
-  - Access to a shared bathroom, conveniently located nearby and equipped with modern fixtures and amenities..<br/>
-</p>
-<h1 className='app__rooms-h1'style={{margin: '7rem 0'}}>Large Single Bedroom</h1>
-     <p className='p__opensans' style={{margin: '-7rem 0'}}>
-  - The large single bedroom offers ample space for relaxation and personal activities.<br />
-  - It features a comfortable and spacious bed, providing a cozy sleeping environment.<br />
-  - The room design incorporates natural light, with large windows that provide a bright and airy atmosphere.<br />
-  - The layout offers sufficient space for additional furniture such as a bookshelf, desk, or cozy armchair.<br />
-  - Includes a comfortable seating area, perfect for reading or unwinding.
-  
-</p>
-<h1 className='app__rooms-h1' style={{margin: '9rem 0'}}>Garden</h1>
-     <p className='p__opensans' style={{margin: '-9rem 0'}}>
-  - Features low-maintenance artificial grass that always looks lush and green.<br />
-  - It includes seating areas or a patio providing a versatile space for outdoor entertaining and relaxation.<br />
-  - The garden design considers lighting options, allowing for enjoyable evenings and enhancing the overall ambiance.<br />
-  - Offers convenient access to dedicated parking spaces located right next to the house, ensuring easy and hassle-free parking for residents and guests.<br />
-</p>
+  <h1 className='app__header-h1' style={{margin: "2rem 0"}}>Living Room</h1>
+  <ul className="app__bullet-list">
+  <li className="p__opensans">
+     A vast living/family room with a large window that allows ample natural light to fill the space
+    </li>
+    <li className="p__opensans">
+     This room offers plenty of room for relaxation, entertainment, and quality time with loved ones.
+    </li>
+    <li className="p__opensans">
+     The large window provides scenic views and enhances the overall ambiance of the room.
+    </li>
+    <li className="p__opensans">
+    The spacious dining room is conveniently located and easily accessible from the large kitchen
+    </li>
+    <li className="p__opensans">
+    It offers a comfortable and inviting space for family meals, gatherings, and entertaining guests.
+    </li>
+    <li className="p__opensans">
+     This combination creates a cohesive and functional area for dining and culinary activities.
+  </li>
+  </ul>
+
+  <h1 className="app__header-h1" style={{ margin: "2rem 0" }}>Kitchen</h1>
+<ul className="app__bullet-list">
+  <li className="p__opensans">
+    The kitchen units are designed to be spacious, providing ample storage and workspace.
+  </li>
+  <li className="p__opensans">
+    Top quality, built with attention to detail and using high-quality materials.
+  </li>
+  <li className="p__opensans">
+    Incorporated with the latest technology, such as advanced appliances and smart features.
+  </li>
+  <li className="p__opensans">
+    Custom lighting fixtures are installed, offering both ambient and task lighting to enhance its functionality.
+  </li>
+</ul>
+
+<h1 className='app__header-h1'style={{margin: '2rem 0'}}>Master Bedroom</h1>
+<ul className="app__bullet-list">
+  <li className="p__opensans">
+  The Master Bedroom Is Designed To Be A Haven Of Peace And Tranquility.
+  </li>
+  <li className="p__opensans">
+  It Features Clean Lines And A Fresh White Finish, Creating A Sense Of Serenity And Calmness
+  </li>
+  <li className="p__opensans">
+  Large Windows Allow Natural Light To Fill The Room, Creating A Bright And Airy Ambiance During The Day.
+  </li>
+  <li className="p__opensans">
+  Spacious And Provides Maximum Room For Relaxation And Restful Sleep.
+  </li>
+  <li className="p__opensans">
+  It Is Equipped With En-Suite Amenities, Offering Convenient And Private Bathroom Facilities.
+  </li>
+  <li className="p__opensans">
+  The En-Suite Amenities Include Features Such As A Luxurious Bathtub, A Modern Shower, And A Stylish Vanity Area.
+  </li>
+</ul>
+
+<h1 className='app__header-h1' style={{margin: '2rem 0'}}>Double Bedroom</h1>
+<ul className="app__bullet-list">
+  <li className="p__opensans">
+  The Double Bedroom Is Designed With A Modern And Stylish Aesthetic, Featuring Contemporary Furniture And Décor.
+  </li>
+  <li className="p__opensans">
+  The Room Design Incorporates Sound-Absorbing Elements, Reducing Noise And Promoting A Peaceful Environment.
+  </li>
+  <li className="p__opensans">
+  Includes Adjustable Lighting Options, Allowing For Creating Different Moods And Ambiance.
+  </li>
+  <li className="p__opensans">
+  Access To A Shared Bathroom, Conveniently Located Nearby And Equipped With Modern Fixtures And Amenities.
+  </li>
+</ul>
+
+<h1 className='app__header-h1'style={{margin: '3rem 0'}}>Large Single Bedroom</h1>
+<ul className="app__bullet-list">
+  <li className="p__opensans">
+  The Large Single Bedroom Offers Ample Space For Relaxation And Personal Activities.
+  </li>
+  <li className="p__opensans">
+  It Features A Comfortable And Spacious Bed, Providing A Cozy Sleeping Environment.
+  </li>
+  <li className="p__opensans">
+  The Room Design Incorporates Natural Light, With Large Windows That Provide A Bright And Airy Atmosphere.
+  </li>
+  <li className="p__opensans">
+  The Layout Offers Sufficient Space For Additional Furniture Such As A Bookshelf, Desk, Or Cozy Armchair.
+  </li>
+  </ul>
+<h1 className='app__header-h1' style={{margin: '2rem 0'}}>Garden</h1>
+<ul className="app__bullet-list">
+  <li className="p__opensans">
+  Features Low-Maintenance Artificial Grass That Always Looks Lush And Green.
+  </li>
+  <li className="p__opensans">
+  It Includes Seating Areas Or A Patio Providing A Versatile Space For Outdoor Entertaining And Relaxation.
+  </li>
+  <li className="p__opensans">
+  The Garden Design Considers Lighting Options, Allowing For Enjoyable Evenings And Enhancing The Overall Ambiance.
+  </li>
+  <li className="p__opensans">
+  Offers Convenient Access To Dedicated Parking Spaces Located Right Next To The House, Ensuring Easy And Hassle-Free Parking For Residents And Guests.
+  </li>
+  </ul>
 </div>
 
    

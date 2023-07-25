9eece8e834b3e4aef35c9cc1cf672f29e5c32031
Henry-Oresegun45 <87371608+Henry-Oresegun@users.noreply.github.com>
2023-07-25 14:50:25 +0100

    Add files via upload

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,31 +1,57 @@
 import bg from '../assets/bg.png';
 import fbathroom from '../assets/fbathroom.png'
-import bath1 from '../assets/bath1.png';
-import bath4 from '../assets/bath4.png';
+import fbathroom1 from '../assets/fbathroom1.png'
+import fbathroom3 from '../assets/fbathroom3.png'
 import garden from '../assets/garden.png';
 import logo from '../assets/logo.png';
 import airbnb from '../assets/airbnb.png';
-import masterbedroom from '../assets/masterbedroom.png';
-import ensuite from '../assets/ensuite.png'
-import doublebedroom from '../assets/doublebedroom.png'
-import largesinglebedroom from '../assets/largesinglebedroom.png'
-import livingroom from '../assets/livingroom.png'
-import hallway from '../assets/hallway.png'
-import kitchen from '../assets/kitchen.png';
+import masterbedroom1 from '../assets/masterbedroom1.png';
+import masterbedroom2 from '../assets/masterbedroom2.png';
+import masterbedroom4 from '../assets/masterbedroom4.png';
+import ensuite from '../assets/ensuite.png';
+import ensuite1 from '../assets/ensuite1.png';
+import doublebedroom from '../assets/doublebedroom.png';
+import doublebedroom2 from '../assets/doublebedroom2.png';
+import doublebedroom3 from '../assets/doublebedroom3.png';
+import doublebedroom4 from '../assets/doublebedroom4.png';
+import largesinglebedroom1 from '../assets/largesinglebedroom1.png';
+import largesinglebedroom2 from '../assets/largesinglebedroom2.png';
+import largesinglebedroom3 from '../assets/largesinglebedroom3.png';
+import largesinglebedroom4 from '../assets/largesinglebedroom4.png';
+import livingroom from '../assets/livingroom.png';
+import hallway from '../assets/hallway.png';
+import hallway2 from '../assets/hallway2.png';
+import hallway3 from '../assets/hallway3.png';
+import kitchen1 from '../assets/kitchen1.png';
+import kitchen2 from '../assets/kitchen2.png';
+import kitchen3 from '../assets/kitchen3.png';
 
 const images = {
     bg,
     fbathroom,
-    bath1,
-    bath4,
+    fbathroom1,
+    fbathroom3,
     garden,
-    masterbedroom,
+    masterbedroom1,
+    masterbedroom2,
+    masterbedroom4,
     ensuite,
+    ensuite1,
     doublebedroom,
-    largesinglebedroom,
+    doublebedroom2,
+    doublebedroom3,
+    doublebedroom4,
+    largesinglebedroom1,
+    largesinglebedroom2,
+    largesinglebedroom3,
+    largesinglebedroom4,
     livingroom,
     hallway,
-    kitchen,
+    hallway2,
+    hallway3,
+    kitchen1,
+    kitchen2,
+    kitchen3,
     logo,
     airbnb
   };
